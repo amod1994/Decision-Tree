9faf9b63c02dc2fd2f8354e7e534c43810199d01
Amod Gandhe <amodgandhes@gmail.com>
2018-05-15 21:51:22 -0400

    asdf

--- Decision-Tree/src/Main.java
+++ Decision-Tree/src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -29,15 +31,65 @@ public class Main {
 		
 		for (BranchDecision branch : curr.branches) {
 			if(branch.partitionedDataSet.data.isEmpty()) {
-				
+				return new Node(dataset.majorityClass);
+			}
+			else {
+				branch.resultant = buildTree(branch.partitionedDataSet, attrList);
 			}
 		}
 		
-		return null;
+		return curr;
 	}
 
 	private static Node attrSelectionMethod(DataSet dataset, List<Integer> attrList) {
-		// TODO Auto-generated method stub
+
+		Double infoD = 0.0;
+		for (String c : dataset.classes.keySet()) {
+			double temp = dataset.classes.get(c)/dataset.data.size();
+			infoD -= temp*Math.log(temp); 
+		}
+		
+		Double maxGain = Double.MIN_VALUE;
+		int selecteedAttr = -1;
+		for (Integer i : attrList) {
+			Map<String, Integer> attrClassCount = new HashMap();
+			Map<String, DataSet> djs = new HashMap();
+			if (attrList.indexOf(i) == attrList.size() - 1)	break;
+			for (String[] str : dataset.data) {
+				if (!djs.containsKey(str[i])) {
+					djs.put(str[i], new DataSet());
+				}
+				djs.get(str[i]).data.add(str);
+				
+				String tAtCl = str[i] + "," + str[attrList.size() - 1];
+				if (!attrClassCount.containsKey(tAtCl)) {
+					attrClassCount.put(tAtCl, 1);
+				} else {
+					attrClassCount.put(tAtCl, attrClassCount.get(tAtCl) + 1);
+				}
+			}
+			Double infoDA = 0.0;
+			for (String dj : djs.keySet()) {
+				double weight = djs.get(dj).data.size() / dataset.data.size();
+				Double infoDj = 0.0;
+				for (String c : dataset.classes.keySet()) {
+					String tAtCl = dj + "," + c;
+					if(attrClassCount.containsKey(tAtCl)) {
+						double temp = attrClassCount.get(tAtCl)/djs.get(dj).data.size();
+						infoDj -= temp*Math.log(temp); 
+					}
+				}
+				infoDA += weight * infoDj;
+			}
+			if(infoD - infoDA > maxGain) {
+				maxGain = infoD - infoDA;
+				selecteedAttr = i;
+			}
+		}
+
+		
+		
+		
 		return null;
 	}
 }
